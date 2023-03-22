9e4484f9ba04b9c140ad809b8cca57d1951507c0
Wouter Nagtegaal <woutnagtegaal@gmail.com>
2023-03-22 11:29:24 +0100

    bal laten stuiteren om de bal binnen het scherm te kunnen houden

--- src/main/java/com/github/hanyaeger/tutorial/entities/Bal.java
+++ src/main/java/com/github/hanyaeger/tutorial/entities/Bal.java
@@ -3,11 +3,12 @@ package com.github.hanyaeger.tutorial.entities;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.entities.SceneBorderCrossingWatcher;
+import com.github.hanyaeger.api.entities.SceneBorderTouchingWatcher;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 import com.github.hanyaeger.api.scenes.SceneBorder;
 import com.github.hanyaeger.tutorial.BreakOutGame;
 
-public class Bal extends DynamicSpriteEntity implements SceneBorderCrossingWatcher {
+public class Bal extends DynamicSpriteEntity implements SceneBorderTouchingWatcher {
     private final BreakOutGame breakOutGame;
     private int grootte = 10;
 
@@ -15,14 +16,32 @@ public class Bal extends DynamicSpriteEntity implements SceneBorderCrossingWatch
 //        super("sprites/ufobalk.png", location, new Size(800, 40));
         super("sprites/bal.png", new Coordinate2D(400, 400), new Size(50, 50));
         int location = 1;
+        setMotion(5, 45);
 
         this.breakOutGame = breakOutGame;
     }
 
-
     @Override
-    public void notifyBoundaryCrossing(SceneBorder sceneBorder) {
+    public void notifyBoundaryTouching(SceneBorder sceneBorder) {
+        /*
+        switch (sceneBorder) {
+            case TOP -> {
+                setAnchorLocationY(1);
+            }
+            case BOTTOM -> {
+                setAnchorLocationY(getSceneHeight() - getHeight() - 1);
+            }
+            case LEFT -> {
+                setAnchorLocationX(1);
+            }
+            case RIGHT -> {
+                setAnchorLocationX(getSceneWidth() - getWidth() - 1);
+            }
+        }
 
+         */
+
+        setDirection((getDirection() + 90) % 360);
     }
 }
 
